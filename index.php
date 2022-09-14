d100613e93c73f9914f8055d7d3dc1eb382877b3
LiMayKo <112835841+LiMayKo@users.noreply.github.com>
2022-09-14 19:02:28 +0800

    index for in/out/final

--- index.php
+++ index.php
@@ -1,82 +1,108 @@
-<?php  
+<?php 
 
+/*class SalesComission {
+  public $product_name;
+  public $quantity;
+  public $price;
 
-class Calculator
-{
-	private $km, $ms, $operator, $output;
+  public $discount = 0;
+  public $discounted_price;
 
-	public function setNumbers($km, $ms)
-	{
-		$this->km = $km;
-		$this->ms = $ms;
-	}
+  public $net_amount;
 
-	public function setOperator($operator)
-	{
-		$this->operator = $operator;
-	}
+  function __construct($product_name, $quantity, $price) {
+    $this->product_name = $product_name;
+    $this->quantity = $quantity;
+    $this->price = $price; 
+  }
 
-	public function calculate()
-	{
-		if ($this->operator == "*") {
-			$this->output = $this->km * $this->ms;
-		}
-		
-	}
+  public function calculateAmountDue() {
+  	$this -> net_amount = $this-> quantity * $this-> price;
+    return $this -> net_amount;
+  }
 
-	public function getOutput()
-	{
-		return $this->output;
-	}
+  public function setDiscount($d) {
+  	$this -> discount = $d;
+    $this-> discounted_price = $this-> net_amount - $this-> discount;
+  }
+
+  public function displaySalesInfo() {
+    echo "Product: " . $this->product_name . "<br>";
+    echo "Qty: " . $this->quantity . "<br>";
+    echo "Price: " . $this->price . "<br>";
+
+    echo "-----------------" . "<br>";
+
+    echo "Amount Due: {$this->calculateAmountDue()}" . "<br>";
+    echo "Discount: {$this->discount}" . "<br>";
+    echo "Net Due: {$this->discounted_price}";
+    // echo $this->net_amount;
+    // echo $this->discount;
+
+  }
+  
 }
 
+$sale_1 = new SalesComission("Sugar 25KG", 2, 1300);
+$sale_1-> calculateAmountDue();
+$sale_1 -> setDiscount(600);
+$sale_1 -> displaySalesInfo();
 
-$km=$_POST['km'];
-	
-$ms = 0.62137119;
-$operator = "*";
+echo "<br>" . "<br>" . "-----------------" . "<br>" . "<br>";;
 
-$calculator = new Calculator();
-$calculator->setNumbers($km,$ms);
-$calculator->setOperator($operator);
-$calculator->calculate();
-$calculator->getOutput();
+$sale_2 = new SalesComission("Sugar 25KG", 2, 1300);
+$sale_2 -> calculateAmountDue();
+$sale_2 -> displaySalesInfo();*/
 
+class store{
+  public $in_qty;
+  public $in_ucost;
+  public $in_value;
+
+  public $out_qty;
+  public $out_ucost;
+  public $out_value;
+
+  public $f_qty;
+  public $f_ucost;
+  public $f_value;
+
+  function __construct( 
+                        $f_qty, $f_ucost, $f_value) {
+    
+    $this->f_qty = $f_qty;
+    $this->f_ucost = $$f_ucost;
+    $this->f_value = $f_value; 
+  }
+
+  public function sale(){
+    $this -> f_qty = $this-> f_qty - $this-> out_qty;
+    $this -> f_value = $this-> f_value - $this-> out_value;
+
+
+    echo "final: quantity".f_qty. "u cost".f_ucost. " value".f_value;
+    
+
+  }
+  public function purchase(){
+    $this -> f_qty = $this-> f_qty + $this-> in_qty;
+    $this -> f_ucost = $this-> f_value + $this-> in_value / $this-> f_qty;
+    $this -> f_value = $this-> f_qty * $this-> f_ucost;
+
+
+    echo "final: quantity".f_qty. "u cost".f_ucost. " value".f_value;
+    
+
+  }
+
+  public function displaySalesInfo() {
+    echo "Sale: {$this->sale()}" . "<br>";
+    echo "Purchase: {$this->purchase()}" . "<br>";
+
+  }
+
+  $sale_1 = new store(1000, 5, 5000);
+  $sale_1 -> displaySalesInfo();
+}
 
 ?>
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>PHP program to Convert Kilometers into Miles</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-  <div class="container">
-      <div class="row">
-          <div class="col">
-              <form method="post" action="">
-              <br><h2>Convert Kilometers into Miles</h2><br>
-             
-              <div class="form-group">
-                  <label for="kms"><h4>Enter Kms</h4></label>
-                  <input type="text" min="0" value="<?=(isset($kms)) ? $km : '';?>" name="km" class="form-control" pattern="[0-9]*" title="Please enter only numbers">
-                  <small id="helpId" class="text-muted">Enter the Kms in numbers and click the button to Convert it</small>
-              </div>
-              <div class="form-group">
-                  <h5>Result:<span id="res"> <?php echo $km." ".$operator." ".$ms. " = ". $calculator->getOutput(); ?> </span></h5>
-              </div> 
-              <div class="form-group">
-                  <button type="submit" name="convert" class="btn btn-primary">Convert to Miles</button>
-              </div>
-              </form>
-          </div>
-      </div>
-    </div>
-  </body>
-</html>
